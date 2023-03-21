f0170d8c210e4f4a4d02f30f64e52b79a92a9301
Hao C <impyworm@gmail.com>
2023-03-20 22:28:39 -0600

    make changes to syntax analysis

--- Syntax Analysis/syntaxAnalysis.cpp
+++ Syntax Analysis/syntaxAnalysis.cpp
@@ -1,24 +1,29 @@
 // AKA parser
 
 // change this later to make code more optimized
+#include <bits/stdc++.h>
 using namespace std;
+
+class Node {
+public:
+    string val;
+
+    Node(string val) {
+        this->val = val;
+        
+        return;
+    }
+};
 
 class ParseTree {
 public:
     ParseTree() {
-        
-    }
-};
-
-class Parser {
-public:
-    Parser() {
 
     }
-
-    // ParseTree* 
 };
 
 int main() {
+    ParseTree* ast = new ParseTree()
+
     return 0;
 }
