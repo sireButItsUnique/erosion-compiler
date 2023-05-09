cd9679a8a28e6b79bf8c07367d45d60ce5cb4e6a
sireButItsUnique <robertcxlee@gmail.com>
2023-05-09 01:03:53 +0000

    added updateCompleteness

--- SyntaxAnalysis/ParseNode.cpp
+++ SyntaxAnalysis/ParseNode.cpp
@@ -1,87 +1,104 @@
 #include "ParseNode.hpp"
 
 ParseNode::ParseNode(Lexer* lexer) {
-    // actual lexer
-    this->lexer = lexer;
+	// actual lexer
+	this->lexer = lexer;  
 
-    // variables for the lex token it represents
-    this->type = "<program>";
-    this->val = "";
+	// variables for the lex token it represents
+	this->type = "<program>";
+	this->val = "";
 
-    // variables to help during construction of tree
-    this->complete = false;
-    this->children = vector<ParseNode*>(0);
-    this->whitelist = {0, 1, 2, 3}; 
+	// variables to help during construction of tree
+	this->complete = false;
+	this->children = vector<ParseNode*>(0);
+	this->whitelist = {0, 1, 2, 3}; 
 }
 
 ParseNode::ParseNode(Lexer* lexer, string type) {
-    // actual lexer
-    this->lexer = lexer;
+	// actual lexer
+	this->lexer = lexer;
 
-    // variables for the lex token it represents
-    this->type = type;
-    this->val = "";
+	// variables for the lex token it represents
+	this->type = type;
+	this->val = "";
 
-    // variables to help during construction of tree
-    this->complete = false;
-    this->children = vector<ParseNode*>(0);
-    this->whitelist = vector<int>(0);
+	// variables to help during construction of tree
+	this->complete = false;
+	this->children = vector<ParseNode*>(0);
+	this->whitelist = vector<int>(0);
 
-    for (int i = 0; i < rules.at(type).size(); i++) {
-        this->whitelist.push_back(i);
-    }
+	for (int i = 0; i < rules.at(type).size(); i++) {
+		this->whitelist.push_back(i);
+	}
 }
 
 ParseNode::ParseNode(Lexer* lexer, string type, string val) {
-    // actual lexer
-    this->lexer = lexer;
+	// actual lexer
+	this->lexer = lexer;
 
-    // variables for the lex token it represents
-    this->type = type;
-    this->val = val;
+	// variables for the lex token it represents
+	this->type = type;
+	this->val = val;
 
-    // variables to help during construction of tree
-    this->complete = false;
-    this->children = vector<ParseNode*>(0);
-    this->whitelist = vector<int>(0); 
+	// variables to help during construction of tree
+	this->complete = false;
+	this->children = vector<ParseNode*>(0);
+	this->whitelist = vector<int>(0); 
 
-    for (int i = 0; i < rules[type].size(); i++) {
-        this->whitelist.push_back(i);
-    }
+	for (int i = 0; i < rules[type].size(); i++) {
+		this->whitelist.push_back(i);
+	}
 }
 
 void ParseNode::updateWhitelist() {
 
-    // looping through each term currently under the node
-    for (int i = 0; i < this->children.size(); i++) {
-        
-        // looping through each term in the remaining whitelist for the current node
-        // in for (...;...;...) form in order to use std::vector.erase() method
-        for (int j = 0; j < this->whitelist.size(); j++) {
-            
-            // current variation in the rules as provided by the whitelist
-            int variation = this->whitelist[j];
-            
-            //checking if there are more terms under the node than there are terms in the variation, if so remove
-            if (this->children.size() > rules[this->type][variation].size()) {
-                this->whitelist.erase(this->whitelist.begin() + j);
-            } 
-            
-            //test whether or not the terms under the node and the terms in the variation match
-            else {
-                string ruleType = rules[this->type][variation][i];
-                string childType = this->children[i]->type;
+	// looping through each term currently under the node
+	for (int i = 0; i < this->children.size(); i++) {
+		
+		// looping through each term in the remaining whitelist for the current node
+		// in for (...;...;...) form in order to use std::vector.erase() method
+		for (int j = 0; j < this->whitelist.size(); j++) {
+			
+			// current rule variation that's currently still considered valid in the whitelist
+			int variation = this->whitelist[j];
+			
+			// checking if there are more terms under the node than there are terms in the variation, if so remove
+			if (this->children.size() > rules[this->type][variation].size()) {
+				this->whitelist.erase(this->whitelist.begin() + j);
+			} 
+			
+			// test whether or not the terms under the node and the terms in the variation match
+			else {
+				string ruleType = rules[this->type][variation][i];
+				string childType = this->children[i]->type;
 
-                if (ruleType != childType) {
-                    this->whitelist.erase(this->whitelist.begin() + j);
-                } 
-            }
-        }
-    }
+				if (ruleType != childType) {
+					this->whitelist.erase(this->whitelist.begin() + j);
+				} 
+			}
+		}
+	}
 }
 
 void ParseNode::updateCompleteness() {
-    
+	// whether all the children are complete
+	for (const auto &child : children) {
+		if (!child->complete) {
+			child->updateCompleteness();
+			if (!child->complete) {
+				return;
+			}
+		}
+	}
+
+	// whether it has the correct number of children/terms
+	if (rules[type][whitelist[0]].size() != children.size()) {
+		return;
+	}
+
+	// marking as complete
+	complete = true; 
+	return;   
 }
 
 bool ParseNode::findPath(SyntaxToken* token, vector<int>& res) {
@@ -101,7 +118,7 @@ void ParseNode::build() {
 }
 
 void ParseNode::print() {
-    for (int i = 0; i < 0; i++) {
+	for (int i = 0; i < 0; i++) {
 		cout << "  ";
 	}
 	cout << this->type;
@@ -120,23 +137,23 @@ void ParseNode::print() {
 }
 
 void ParseNode::print(int depth) {
-    for (int i = 0; i < depth; i++) {
-        cout << "  ";
-    }
-    
-    cout << this->type;
-    if (this->val != "") {
-        cout << ": " << this->val;
-    }
+	for (int i = 0; i < depth; i++) {
+		cout << "  ";
+	}
+	
+	cout << this->type;
+	if (this->val != "") {
+		cout << ": " << this->val;
+	}
 
-    if (this->complete) {
-        cout << " (complete)";
-    }
+	if (this->complete) {
+		cout << " (complete)";
+	}
 
-    cout << endl;
-    for (auto& c : this->children) {
-        c->print(depth + 1);
-    }
+	cout << endl;
+	for (auto& c : this->children) {
+		c->print(depth + 1);
+	}
 
 	return;
 }
